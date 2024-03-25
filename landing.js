940999b4af35f98097f2b5036f2d847b69aa8a44
Sayan Roy <100018672+D3FaltXD@users.noreply.github.com>
2024-03-25 23:33:13 +0530

    removed carousel

--- landing.js
+++ landing.js
@@ -21,34 +21,7 @@ form.addEventListener('submit', (event) => {
 
 
 // Text Carousel
-const carousel = document.querySelector('.carousel-slides');
-const slides = carousel.querySelectorAll('.slide');
-const prevButton = document.querySelector('.prev-button');
-const nextButton = document.querySelector('.next-button');
 
-let currentSlide = 0;
-
-function moveSlide(slideIndex) {
-  carousel.style.transform = `translateX(-${slideIndex * 100}%)`;
-  currentSlide = slideIndex;
-}
-
-nextButton.addEventListener('click', () => {
-  const totalSlides = slides.length;
-  if (currentSlide === totalSlides - 1) {
-    moveSlide(0); // Move to first slide on next click
-  } else {
-    moveSlide(currentSlide + 1);
-  }
-});
-
-prevButton.addEventListener('click', () => {
-  if (currentSlide === 0) {
-    moveSlide(totalSlides - 1); // Move to last slide on prev click
-  } else {
-    moveSlide(currentSlide - 1);
-  }
-});
 
 
 //Parallax starts here
@@ -58,7 +31,7 @@ window.addEventListener('scroll', function() {
 const scrollTop = window.scrollY;
 
 parallaxLayers.forEach((layer, index) => {
-  const speed = index === 0 ? -0.1 : index === 0 ? 0.1 : 0.; // Adjust speed as needed
+  const speed = index === 0 ? -0.1 : index === 0 ? 0.01 : 0.1; // Adjust speed as needed
   const transformY = scrollTop * speed;
   layer.style.transform = `translateY(${transformY}px)`;
 });
